75ac704735138a6d6bc11eb5a0b0035278f94da8
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-13 22:06:17 +0100

    fix: synced tailwind breakpoints with ui library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xs: "520px",
+        sm: "768px",
+        md: "1024px",
+        lg: "1280px",
+        xl: "1640px",
+      },
     },
   },
   plugins: [],
