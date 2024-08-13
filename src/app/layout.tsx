ce4a333b835e114bcedbccb5240ca275c27c6e7c
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-13 23:05:18 +0100

    feat: add custom icons to toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import Header from "@/components/header";
+import {
+  CheckCircledIcon,
+  CrossCircledIcon,
+  ExclamationTriangleIcon,
+  InfoCircledIcon,
+} from "@radix-ui/react-icons";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
@@ -34,8 +40,16 @@ export default function RootLayout({
             <Theme>
               <AnalyticsPageView />
               <Header />
+
               {children}
+
               <Toaster
+                icons={{
+                  success: <CheckCircledIcon color="green" />,
+                  info: <InfoCircledIcon color="blue" />,
+                  warning: <ExclamationTriangleIcon color="orange" />,
+                  error: <CrossCircledIcon color="red" />,
+                }}
                 toastOptions={{
                   style: {
                     background: "var(--color-background)",
