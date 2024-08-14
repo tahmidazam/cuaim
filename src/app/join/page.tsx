dab63ac2d39a2776111c07f64e6aa778706ff442
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-14 10:17:32 +0100

    feat: add join mailing list page

---
+++ src/app/join/page.tsx
@@ -0,0 +1,38 @@
+import JoinMailingListForm from "@/components/join-mailing-list-form";
+import Md from "@/components/md";
+import {
+  Container,
+  Flex,
+  Heading,
+  Section,
+  Separator,
+  Spinner,
+  Text,
+} from "@radix-ui/themes";
+import { Suspense } from "react";
+
+/**
+ * A page to join the mailing list.
+ */
+export default async function Join() {
+  return (
+    <Section>
+      <Container size="1" className="px-2">
+        <Flex direction="column" className="gap-8">
+          <Flex direction="column" className="gap-2">
+            <Heading>Join the mailing list</Heading>
+            <Text>Get lecture and event updates straight to your inbox.</Text>
+          </Flex>
+
+          <JoinMailingListForm />
+
+          <Separator className="w-full" />
+
+          <Suspense fallback={<Spinner />}>
+            <Md url="public/join.mdx" textSize="1" textColour="gray" />
+          </Suspense>
+        </Flex>
+      </Container>
+    </Section>
+  );
+}
