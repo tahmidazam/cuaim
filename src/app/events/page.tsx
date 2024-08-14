ce178a1d45b6e8385c960f5db0e29f7bd9869233
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-14 18:56:43 +0100

    feat: add event page and loading state

---
+++ src/app/events/page.tsx
@@ -0,0 +1,12 @@
+import Events from "@/components/events";
+import { Container, Section } from "@radix-ui/themes";
+
+export default function EventsPage() {
+  return (
+    <Section>
+      <Container>
+        <Events />
+      </Container>
+    </Section>
+  );
+}
