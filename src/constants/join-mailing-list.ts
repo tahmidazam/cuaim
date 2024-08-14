4eca565c0a46bf3806be70534e4c8987a7f93275
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-14 18:55:39 +0100

    refactor: change name of mailing list base url constant

--- src/constants/join-mailing-list.ts
+++ src/constants/join-mailing-list.ts
@@ -1,2 +1,3 @@
-export const BASE_URL: string = "https://lists.cam.ac.uk/sympa/subscribe";
+export const MAILING_LIST_BASE_URL: string =
+  "https://lists.cam.ac.uk/sympa/subscribe";
 export const LIST_NAME: string = "soc-aim";
