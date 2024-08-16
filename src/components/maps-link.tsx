dd96632c576143261bfb63a5f7693fecf6922122
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-16 08:00:59 +0100

    feat: add map link

---
+++ src/components/maps-link.tsx
@@ -0,0 +1,33 @@
+import { mappingProviders } from "@/constants/mapping-providers";
+import { LabelledHref } from "@/interfaces/labelled-href";
+import { DropdownMenu, Link as RadixLink } from "@radix-ui/themes";
+import Link from "next/link";
+
+/**
+ * A link with options to open the location with a dropdown menu of mapping
+ * providers.
+ * @param param0 The location to link to.
+ */
+export default function MapsLink({ location }: { location?: string }) {
+  // If there is no location, return null.
+  if (!location) return null;
+
+  // Encode the location for use in a URL.
+  const encodedLocation: string = encodeURIComponent(location);
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <RadixLink href="#">{location}</RadixLink>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content>
+        {mappingProviders.map(({ label, href: baseUrl }: LabelledHref) => (
+          <DropdownMenu.Item asChild>
+            <Link href={`${baseUrl}${encodedLocation}`}>{label}</Link>
+          </DropdownMenu.Item>
+        ))}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}
