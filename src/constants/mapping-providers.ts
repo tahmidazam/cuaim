54e9862370eeb663722beb30893cdc07961d2b9c
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-16 08:00:59 +0100

    feat: add map link

---
+++ src/constants/mapping-providers.ts
@@ -0,0 +1,12 @@
+import { LabelledHref } from "@/interfaces/labelled-href";
+
+export const mappingProviders: LabelledHref[] = [
+  {
+    href: "https://beta.maps.apple.com/?q=",
+    label: "Open in Apple Maps",
+  },
+  {
+    href: "http://maps.google.com/?q=",
+    label: "Open in Google Maps",
+  },
+];
