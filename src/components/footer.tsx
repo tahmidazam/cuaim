31037aa134d1819fa5eddc74c24be32345b82985
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-17 22:01:51 +0100

    feat: update footer with contact information

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { contactLabelledHrefs } from "@/constants/contact-labelled-hrefs";
 import { tabs } from "@/constants/tabs";
 import { LabelledHref } from "@/interfaces/labelled-href";
 import { Container, Flex, Link as RadixLink, Text } from "@radix-ui/themes";
@@ -12,15 +13,31 @@ export default function Footer() {
     <Flex className=" bg-[var(--gray-2)]">
       <Container>
         <Flex className="flex-col p-4 gap-8">
-          <Flex className="flex-col gap-2">
-            <Text>Sitemap</Text>
+          <Flex className="gap-24 flex-wrap">
+            <Flex className="flex-col gap-2">
+              <Text>Sitemap</Text>
 
-            <Flex className="flex-col">
-              {tabs.map(({ href, label }: LabelledHref, index: number) => (
-                <RadixLink key={index} asChild size="2">
-                  <Link href={href}>{label}</Link>
-                </RadixLink>
-              ))}
+              <Flex className="flex-col">
+                {tabs.map(({ href, label }: LabelledHref, index: number) => (
+                  <RadixLink key={index} asChild size="2">
+                    <Link href={href}>{label}</Link>
+                  </RadixLink>
+                ))}
+              </Flex>
+            </Flex>
+
+            <Flex className="flex-col gap-2">
+              <Text>Contact</Text>
+
+              <Flex className="flex-col">
+                {contactLabelledHrefs.map(
+                  ({ href, label }: LabelledHref, index: number) => (
+                    <RadixLink key={index} asChild size="2">
+                      <Link href={href}>{label}</Link>
+                    </RadixLink>
+                  )
+                )}
+              </Flex>
             </Flex>
           </Flex>
 
