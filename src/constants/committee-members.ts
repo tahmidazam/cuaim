103260729be21bd5f04254fc80fad41e524f4cc3
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-17 21:35:43 +0100

    feat: update committee information

--- src/constants/committee-members.ts
+++ src/constants/committee-members.ts
@@ -19,10 +19,14 @@ export const committeeMembers: CommitteeMember[] = [
     role: "Engineering Representative",
   },
   {
+    name: "Harshvir Grewal",
+    role: "Events Officer",
+  },
+  {
     name: "Keshav Kedia",
     role: "Events Officer",
     headline: "MedST IB",
-    bio: '*"I\'m a second year medic at Caius fascinated to explore how AI will interface with doctors in future healthcare delivery. I will be helping to organise events and socials for the society throughout the coming year!"*',
+    bio: '> *"I\'m a second year medic at Caius fascinated to explore how AI will interface with doctors in future healthcare delivery. I will be helping to organise events and socials for the society throughout the coming year!"*',
     contact: [
       {
         href: "mailto:kk814@cam.ac.uk",
@@ -31,13 +35,10 @@ export const committeeMembers: CommitteeMember[] = [
     ],
   },
   {
-    name: "Harshvir Grewal",
-    role: "Events Officer",
-  },
-  {
     name: "Tahmid Azam",
     role: "Webmaster",
     headline: "MedST IB",
+    bio: '> *"I\'m a second year medic at Emmanuel passionate about applications of ML to clinical practice, particularly in the disabled community."*',
     contact: [
       {
         href: "mailto:ta549@cam.ac.uk",
