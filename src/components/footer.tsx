446b041693a10d14bdb64afde06a3a51cd96a3d8
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-18 19:34:19 +0100

    refactor: add footer section component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,12 @@
 import { contactLabelledHrefs } from "@/constants/contact-labelled-hrefs";
 import { tabs } from "@/constants/tabs";
 import { LabelledHref } from "@/interfaces/labelled-href";
-import {
-  Button,
-  Container,
-  Dialog,
-  Flex,
-  Link as RadixLink,
-  Section,
-  Separator,
-  Text,
-} from "@radix-ui/themes";
+import { Container, Flex, Link as RadixLink, Section } from "@radix-ui/themes";
 import Link from "next/link";
-import Md from "./md";
-import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import AnalyticsInfoDialog from "./analytics-info-dialog";
+import FooterSection from "./footer-section";
+import Md from "./md";
+import ThemeSegmentedControl from "./theme-segmented-control";
 
 /**
  * The website footer.
@@ -23,41 +15,33 @@ export default function Footer() {
   return (
     <Section className="bg-[var(--gray-2)]" py="0">
       <Container>
-        <Flex className="flex-col p-4 gap-8">
-          <Flex className="gap-8 flex-wrap">
-            <Flex className="flex-col gap-2 flex-grow">
-              <Text>Sitemap</Text>
+        <Flex className="flex-col p-4 gap-8 items-start">
+          <Flex className="justify-end w-full md:hidden">
+            <ThemeSegmentedControl size="1" radius="full" />
+          </Flex>
 
-              <Flex className="flex-col ">
-                {tabs.map(({ href, label }: LabelledHref, index: number) => (
+          <Flex className="gap-8 flex-wrap">
+            <FooterSection title="Sitemap">
+              {tabs.map(({ href, label }: LabelledHref, index: number) => (
+                <RadixLink key={index} asChild size="2">
+                  <Link href={href}>{label}</Link>
+                </RadixLink>
+              ))}
+            </FooterSection>
+
+            <FooterSection title="Contact">
+              {contactLabelledHrefs.map(
+                ({ href, label }: LabelledHref, index: number) => (
                   <RadixLink key={index} asChild size="2">
                     <Link href={href}>{label}</Link>
                   </RadixLink>
-                ))}
-              </Flex>
-            </Flex>
+                )
+              )}
+            </FooterSection>
 
-            <Flex className="flex-col gap-2 flex-grow">
-              <Text>Contact</Text>
-
-              <Flex className="flex-col">
-                {contactLabelledHrefs.map(
-                  ({ href, label }: LabelledHref, index: number) => (
-                    <RadixLink key={index} asChild size="2">
-                      <Link href={href}>{label}</Link>
-                    </RadixLink>
-                  )
-                )}
-              </Flex>
-            </Flex>
-
-            <Flex className="flex-col gap-2 flex-grow">
-              <Text>Docs</Text>
-
-              <Flex className="flex-col">
-                <AnalyticsInfoDialog />
-              </Flex>
-            </Flex>
+            <FooterSection title="Docs">
+              <AnalyticsInfoDialog />
+            </FooterSection>
           </Flex>
 
           <Md filename="credits" textColour="gray" textSize="1" />
