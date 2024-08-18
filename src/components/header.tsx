04ac91805b956487f4d14fe6918d932a0f832330
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-18 09:00:03 +0100

    feat: add mobile navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import { LabelledHref } from "@/interfaces/labelled-href";
 import { Box, Flex, TabNav } from "@radix-ui/themes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import MobileTabs from "./mobile-tabs";
 import ThemeSegmentedControl from "./theme-segmented-control";
 
 /**
@@ -18,32 +19,38 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <Box className="sticky top-0 z-10">
-      <TabNav.Root className="bg-[var(--color-background)]" justify="center">
-        <TabNav.Link
-          asChild
-          className="flex sm:absolute sm:left-0 font-bold text-[var(--accent-11)]"
-        >
-          <Link href="/">CU AIM</Link>
-        </TabNav.Link>
-
-        {tabs.map(({ href, label }: LabelledHref) => (
+    <>
+      <Box className="sticky top-0 z-10">
+        <TabNav.Root className="bg-[var(--color-background)]" justify="center">
           <TabNav.Link
-            key={href}
-            active={
-              href === "/" ? pathname === href : pathname.startsWith(href)
-            }
-            className="hidden sm:flex"
             asChild
+            className="flex sm:absolute sm:left-0 font-bold text-[var(--accent-11)]"
           >
-            <Link href={href}>{label}</Link>
+            <Link href="/">CU AIM</Link>
           </TabNav.Link>
-        ))}
 
-        <Flex className="absolute right-0 items-center h-full pr-2 hidden sm:flex">
-          <ThemeSegmentedControl size="1" radius="full" />
-        </Flex>
-      </TabNav.Root>
-    </Box>
+          {tabs.map(({ href, label }: LabelledHref) => (
+            <TabNav.Link
+              key={href}
+              active={
+                href === "/" ? pathname === href : pathname.startsWith(href)
+              }
+              className="hidden sm:flex"
+              asChild
+            >
+              <Link href={href}>{label}</Link>
+            </TabNav.Link>
+          ))}
+
+          <Flex className="absolute right-0 items-center h-full pr-2 hidden sm:flex">
+            <ThemeSegmentedControl size="1" radius="full" />
+          </Flex>
+
+          <Flex className="absolute right-0 items-center h-full pr-4 sm:hidden">
+            <MobileTabs />
+          </Flex>
+        </TabNav.Root>
+      </Box>
+    </>
   );
 }
