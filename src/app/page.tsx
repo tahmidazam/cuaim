9138b74cf554c892b3c9674dff71c1df57c89180
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-18 19:33:17 +0100

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,57 @@
+import Hero from "@/components/hero";
+import { ArrowRightIcon, UpdateIcon } from "@radix-ui/react-icons";
+import { Badge, Button, Flex } from "@radix-ui/themes";
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Hero
+        primary="Learn, collaborate, and network where AI and medicine intersect"
+        secondary="Cambridge University Artifical Intelligence in Medicine Society"
+      >
+        <Flex
+          className="flex-col gap-8 w-full"
+          align={{
+            initial: "start",
+            sm: "center",
+          }}
+        >
+          <Flex
+            className="gap-2 flex-wrap"
+            align={{
+              initial: "start",
+              sm: "center",
+            }}
+          >
+            <Button
+              asChild
+              size={{
+                initial: "2",
+                sm: "4",
+              }}
+            >
+              <Link href="/join">Join mailing list</Link>
+            </Button>
+
+            <Button
+              variant="soft"
+              size={{
+                initial: "2",
+                sm: "4",
+              }}
+            >
+              <UpdateIcon /> Sync calendar
+            </Button>
+          </Flex>
+
+          <Link href="/committee">
+            <Badge size="3" radius="full" variant="surface">
+              🎉 Meet our 2024 Committee <ArrowRightIcon />
+            </Badge>
+          </Link>
+        </Flex>
+      </Hero>
+    </main>
+  );
 }
