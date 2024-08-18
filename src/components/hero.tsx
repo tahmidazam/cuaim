623cc732670489bc9ba54a91e7dff0a749e72bf3
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-18 19:33:17 +0100

    feat: add home page

---
+++ src/components/hero.tsx
@@ -0,0 +1,70 @@
+import { Container, Flex, Heading, Section, Text } from "@radix-ui/themes";
+
+/** The props for the `Hero` component. */
+export interface HeroProps {
+  primary: string;
+  secondary: string;
+  children?: React.ReactNode;
+}
+
+/**
+ * A repsonsive hero section.
+ *
+ * Aligns right on initial breakpoint and center on `sm` and above.
+ * @param param0 The primary, secondary text, and children to render below.
+ */
+export default function Hero({ primary, secondary, children }: HeroProps) {
+  return (
+    <Section className="py-0">
+      <Container size="3" className="px-4">
+        <Flex
+          className="flex-col gap-8 sm:gap-16 items-center justify-center"
+          style={{
+            height: "calc(100vh - 88px)",
+          }}
+          align={{
+            initial: "start",
+            sm: "center",
+          }}
+        >
+          <Flex
+            className="flex-col gap-2 sm:gap-4"
+            align={{
+              initial: "start",
+              sm: "center",
+            }}
+          >
+            <Text
+              size={{
+                initial: "4",
+                sm: "6",
+              }}
+              align={{
+                initial: "left",
+                sm: "center",
+              }}
+              wrap="balance"
+            >
+              {secondary}
+            </Text>
+
+            <Heading
+              size={{
+                initial: "8",
+                sm: "9",
+              }}
+              align={{
+                initial: "left",
+                sm: "center",
+              }}
+            >
+              {primary}
+            </Heading>
+          </Flex>
+
+          {children && children}
+        </Flex>
+      </Container>
+    </Section>
+  );
+}
