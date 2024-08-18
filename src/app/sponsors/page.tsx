55a3e6609ccbef89702f927802ead56264f99dec
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-18 19:35:14 +0100

    feat: add sponsors page

---
+++ src/app/sponsors/page.tsx
@@ -0,0 +1,12 @@
+import Md from "@/components/md";
+import { Container, Section } from "@radix-ui/themes";
+
+export default function SponsorsPage() {
+  return (
+    <Section>
+      <Container size="1">
+        <Md filename="sponsors" />
+      </Container>
+    </Section>
+  );
+}
