94dc2e97c93db22603e8767f487197f94ec2a4b1
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-18 19:34:19 +0100

    refactor: add footer section component

---
+++ src/components/footer-section.tsx
@@ -0,0 +1,23 @@
+import { Flex, Text } from "@radix-ui/themes";
+
+/**
+ * The props for the `FooterSection` component.
+ */
+export interface FooterSectionProps {
+  title: string;
+  children?: React.ReactNode;
+}
+
+/**
+ * A labelled section in the footer.
+ * @param param0 The title and children of the section.
+ */
+export default function FooterSection({ title, children }: FooterSectionProps) {
+  return (
+    <Flex className="flex-col gap-2 flex-grow">
+      <Text>{title}</Text>
+
+      {children && <Flex className="flex-col">{children}</Flex>}
+    </Flex>
+  );
+}
