7c19554fdc732f91a730340df19c92eb7df79ae3
Tahmid Azam <tahmid_azam@icloud.com>
2024-08-23 16:10:16 +0100

    feat: update committee member layout

--- src/components/committee-member-cell.tsx
+++ src/components/committee-member-cell.tsx
@@ -25,36 +25,43 @@ export default function CommitteeMemberCell({
   pfp,
 }: CommitteeMemberCellProps) {
   return (
-    <Flex className="gap-4 hover:bg-[var(--gray-2)] w-full h-full p-4">
-      {pfp ? (
-        <Image src={pfp} alt={name} className="max-w-40" />
-      ) : (
-        <Box className="bg-[var(--gray-3)] w-full max-w-40 h-40 flex-shrink-0"></Box>
-      )}
+    <Flex className="flex-col gap-4 hover:bg-[var(--gray-2)] w-full h-full p-4">
+      <Flex className="gap-4">
+        {pfp ? (
+          <Image
+            src={pfp}
+            alt={name}
+            className="w-40 h-40 object-cover"
+            placeholder="blur"
+          />
+        ) : (
+          <Box className="bg-[var(--gray-3)] w-full max-w-40 h-40 flex-shrink-0"></Box>
+        )}
 
-      <Flex className="flex-col gap-4">
-        <Flex className="flex-col gap-2">
-          <Flex className="flex-col">
-            <Text weight="bold">{name}</Text>
+        <Flex className="gap-4">
+          <Flex className="flex-col gap-2">
+            <Flex className="flex-col">
+              <Text weight="bold">{name}</Text>
 
-            <Text color="gray" size="2">
-              {[role, headline].filter(Boolean).join(" · ")}
-            </Text>
-          </Flex>
-
-          {contact && (
-            <Flex className="gap-2 flex-wrap">
-              {contact.map(
-                (contactLabelledHref: ContactLabelledHref, index: number) => (
-                  <ContactLink key={index} {...contactLabelledHref} />
-                )
-              )}
+              <Text color="gray" size="2">
+                {[role, headline].filter(Boolean).join(" · ")}
+              </Text>
             </Flex>
-          )}
-        </Flex>
 
-        <Md source={bio} textSize="2" />
+            {contact && (
+              <Flex className="gap-2 flex-wrap">
+                {contact.map(
+                  (contactLabelledHref: ContactLabelledHref, index: number) => (
+                    <ContactLink key={index} {...contactLabelledHref} />
+                  )
+                )}
+              </Flex>
+            )}
+          </Flex>
+        </Flex>
       </Flex>
+
+      <Md source={bio} textSize="2" />
     </Flex>
   );
 }
